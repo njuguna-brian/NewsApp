fe878ed24328b6e7ed3f8fba90b7b2e05834b9ea
Brian Njuguna <brian@ibuqacapital.com>
2024-10-15 15:41:28 +0300

    chore: implement datastore preferences

---
+++ app/src/main/java/com/brian/newsapp/domain/manger/LocalUserManger.kt
@@ -0,0 +1,8 @@
+package com.brian.newsapp.domain.manger
+
+import kotlinx.coroutines.flow.Flow
+
+interface LocalUserManger {
+    suspend fun saveAppEntry()
+    fun readAppEntry(): Flow<Boolean>
+}
---
+++ app/src/main/java/com/brian/newsapp/domain/usecases/ReadAppEntry.kt
@@ -0,0 +1,13 @@
+package com.brian.newsapp.domain.usecases
+
+import com.brian.newsapp.domain.manger.LocalUserManger
+import kotlinx.coroutines.flow.Flow
+
+class ReadAppEntry(
+    private val localUserManger: LocalUserManger
+) {
+    suspend operator fun invoke(): Flow<Boolean> {
+        return localUserManger.readAppEntry()
+    }
+
+}
